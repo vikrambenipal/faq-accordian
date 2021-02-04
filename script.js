b38ffb4e293609b0afc99dc7680be7f433bef3c2
vikrambenipal <vikrambenipal@gmail.com>
2021-02-04 14:31:50 -0500

    mobile version finished

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.querySelectorAll('.faq-item').forEach(item => {
     item.addEventListener('click', () => {
+        console.log(item.children[0]);
+        item.children[0].classList.toggle('bold');
         item.children[1].classList.toggle('arrow-active');
         item.children[2].classList.toggle('content-active');
     })
